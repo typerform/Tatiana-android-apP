596b1e480a6fb7e5158dce6b55b2d32bfd7b73a1
typerform <112315719+typerform@users.noreply.github.com>
2023-03-12 14:22:30 +0000

    Back to normal

--- src/component/HeaderComponent.vue
+++ src/component/HeaderComponent.vue
@@ -1,1 +1,18 @@
-<
+<template>
+    <v-app-bar 
+    app 
+    clipped-left
+    >
+        <v-toolbar-title>PLANNING</v-toolbar-title>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/component/content/ContentDomainComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-content>
+        <v-container 
+        fill-height
+        >
+            <app-content-menu></app-content-menu>
+            <app-content-shell></app-content-shell>
+        </v-container>
+    </v-content>
+</template>
+
+<script>
+import ContentMenuComponent from './ContentMenuComponent.vue';
+import ContentShellComponent from './ContentShellComponent.vue';
+
+export default {
+    components: {
+        'app-content-menu':
