11c07910edbe62a41cc52ddc2752be0d7da9f23f
typerform <112315719+typerform@users.noreply.github.com>
2023-03-12 09:38:28 +0000

    fix bad commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+
+<template>
+    <div id="app">
+        <v-app>
+            <header>
+                <app-header></app-header>
+            </header>
+            <aside>
+                <app-menu></app-menu>
+            </aside>
+            <app-content></app-content>
+            <footer>
+                <app-footer></app-footer>
+            </footer>
+            <app-snack-bar></app-snack-bar>
+        </v-app>
+    </div>
+</template>
+
+<script>
+import HeaderComponent from './component/HeaderComponent.vue';
+import MenuComponent from './component/menu/MenuComponent.vue';
+import ContentDomainComponent from './component/content/ContentDomainComponent.vue';
+import FooterComponent from './component/FooterComponent.vue';
+import SnackBarComponent from './component/fixed/SnackBarComponent.vue';
+
+
+export default {
+    components : {
+        'app-header': HeaderComponent,
+        'app-menu': MenuComponent,
+        'app-content': ContentDomainComponent,
+        'app-footer': FooterComponent,
+        'app-snack-bar': SnackBarComponent
+    },
+    created() {
+        this.$vuetify.theme.dark = true
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/component/FooterComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-footer 
+    app
+    >
+        <v-spacer></v-spacer>
+        <div>&copy; 2019</div>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    
+</style>
