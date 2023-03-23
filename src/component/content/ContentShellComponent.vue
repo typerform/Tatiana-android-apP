7c88912dd3c30fa72c3528b8a1d11b14d5b03db8
typerform <112315719+typerform@users.noreply.github.com>
2023-03-23 18:27:41 +0000

    add js file

--- src/component/content/ContentShellComponent.vue
+++ src/component/content/ContentShellComponent.vue
@@ -2,4 +2,34 @@
     <v-row
     style="height : 90%;"
     >
-        <
+        <v-col
+        style="height : 100%;"
+        >
+            <v-sheet
+            width="100%"
+            height="100%"
+
+            >
+                <keep-alive>
+                    <template>
+                        <component :is="componentInstance" />
+                    </template>
+                </keep-alive>
+            </v-sheet>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+import eventBus from '../../utils/eventBus.js';
+
+export default {
+    data() {
+        return {
+            componentIdx : 0
+        }
+    },
+    mounted() {
+        eventBus.$on("getContentIndex", this.changeContentIdx);
+    },
+ 
