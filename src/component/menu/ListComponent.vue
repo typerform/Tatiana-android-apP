5af04039935f0825b4cd3345c56957aa8dcf1d85
typerform <112315719+typerform@users.noreply.github.com>
2023-04-09 07:48:57 +0000

    Add badges to README.md

--- src/component/menu/ListComponent.vue
+++ src/component/menu/ListComponent.vue
@@ -11,4 +11,26 @@
                 </v-list-item-icon>
                 <v-list-item-content>
                     <v-list-item-title v-text="item.title"></v-list-item-title>
-                   
+                    <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list-item-group>
+    </v-list>
+</template>
+
+<script>
+import { mdiPlaylistEdit, mdiCalendarRange } from '@mdi/js';
+import eventBus from '../../utils/eventBus';
+
+export default {
+    data() {
+        return {
+            drawer: true,
+            items: [
+                {
+                    title : "CHECK-LIST",
+                    subtitle: "PLAN YOUR TODAY",
+                    icon : mdiPlaylistEdit
+                },
+                {
+           
