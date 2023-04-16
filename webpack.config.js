a39b64c495eca4587f0661021912e9e8e8ab2b81
typerform <112315719+typerform@users.noreply.github.com>
2023-04-16 10:51:32 +0000

    New release :D

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,22 @@
-const VueLoaderPlugin = require("vue-loader/lib/p
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const webpack = require("webpack");
+
+module.exports = {
+    entry : './src/main.js',
+    module : {
+        rules : [
+            { 
+                test : /\.js$/, 
+                use: [
+                    {
+                        loader : 'babel-loader',
+                        options: {
+                            plugins: ['@babel/plugin-syntax-dynamic-import']
+                        }
+                    }
+         
